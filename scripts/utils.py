274afc79eb8db41c96f9a183d0101bc18a38db92
shuying <zshuying@tamu.edu>
2022-10-03 23:30:24 -0500

    some edits

--- scripts/utils.py
+++ scripts/utils.py
@@ -23,10 +23,18 @@ def map_stringdb_uniprot():
     ppi_stringdb=pd.read_csv('../data/human_ppi_9606.protein.links.full.v11.5.stringdb.txt',sep=" ")
     dic=dict(zip(f['From'],f['Entry']))
     ppi_uniprot=ppi_stringdb.copy()
+    j=0
     for i in range(len(ppi_uniprot)):
-        ppi_uniprot.iloc[i,0]=dic[ppi_stringdb.iloc[i,0]]
-        ppi_uniprot.iloc[i,1]=dic[ppi_stringdb.iloc[i,1]]
+        try:
+            ppi_uniprot.iloc[i, 0] = dic[ppi_stringdb.iloc[i+j, 0]]
+            ppi_uniprot.iloc[i,1]=dic[ppi_stringdb.iloc[i+j,1]]
+        except(KeyError):
+            print('%s not found in uniprot '%ppi_stringdb.iloc[i,1])
+            j+=1
+            continue
+    print('%s rows are deleted'%j)
     ppi_out=ppi_uniprot[['protein1','protein2','experiments','experiments_transferred']]
+
     print(ppi_out.head())
     ppi_out.to_csv('../data/ppi_uniprot.csv',index=False)
 
