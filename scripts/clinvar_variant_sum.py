1d7353d67b2ad73abc528e5e05a04209dd6b3f3b
Shuying Zhu <zshuying@gaozi.ece.tamu.edu>
2023-03-01 18:46:59 -0600

    ,

--- scripts/clinvar_variant_sum.py
+++ scripts/clinvar_variant_sum.py
@@ -5,7 +5,6 @@ import sys
 from utils import *
 
 import pandas as pd
-f=pd.read_csv(os.path.join(data_path,'clinvar','variant_summary_2023_02_26.txt'),sep='\t')
 # f_simple=pd.read_csv('/home/grads/z/zshuying/Documents/shuying/ppi_mutation/scripts/2022-02-variant-sum_remain.csv',index_col='#AlleleID')
 def if_positive_or_negative(string_list):
     label=[]
@@ -64,43 +63,43 @@ def get_uniprot_from_name(name):
         uniprot=find_fasta_refseq(refseq)
     return uniprot 
 
+if __name__ == '__main__':
+    f=pd.read_csv(os.path.join(data_path,'clinvar','variant_summary_2023_02_26.txt'),sep='\t')
+    f['label']=if_positive_or_negative(f['ClinicalSignificance'])
+
+    keep_cols=['#AlleleID', 'Type', 'Name','label', 'ClinicalSignificance', 'ClinSigSimple', 'ReviewStatus', 'OtherIDs', 'LastEvaluated', 'RS# (dbSNP)',
+        'nsv/esv (dbVar)', 'RCVaccession']
+    keep_conditions=(f['Type']=='single nucleotide variant') &\
+                (['no assertion' not in item for item in f['ReviewStatus'].tolist()]) &\
+                (['p' in item for item in f['Name'].tolist()]) &\
+                (f['label']!=0)
+    f_simple=f[keep_conditions][keep_cols]
+    f_simple=f_simple.drop_duplicates(subset=['#AlleleID'])
+
+    f_simple.head()
 
 
-f['label']=if_positive_or_negative(f['ClinicalSignificance'])
-
-keep_cols=['#AlleleID', 'Type', 'Name','label', 'ClinicalSignificance', 'ClinSigSimple', 'ReviewStatus', 'OtherIDs', 'LastEvaluated', 'RS# (dbSNP)',
-       'nsv/esv (dbVar)', 'RCVaccession']
-keep_conditions=(f['Type']=='single nucleotide variant') &\
-              (['no assertion' not in item for item in f['ReviewStatus'].tolist()]) &\
-               (['p' in item for item in f['Name'].tolist()]) &\
-               (f['label']!=0)
-f_simple=f[keep_conditions][keep_cols]
-f_simple=f_simple.drop_duplicates(subset=['#AlleleID'])
-
-f_simple.head()
-
-
-num_processes = multiprocessing.cpu_count()
-print('%s cpus'%num_processes)
-chunk_size = int(f_simple.shape[0]/num_processes)
-# global pbar
-# pbar = tqdm(total=len(f_simple))
-f_simple['UniProt']=[0]*len(f_simple)
-chunks = [f_simple.loc[f_simple.index[i:i + chunk_size]] for i in range(0, f_simple.shape[0], chunk_size)]
-def func(df):
-    mark=df.index[0]
-    print('process %s starts....'%mark)
-    for j,idx in enumerate(df.index):
-        if 'UniProt' not in df.loc[idx,'OtherIDs']:
-                df.loc[idx,'UniProt']=get_uniprot_from_name(f_simple.loc[idx,'Name'])
-        else:df.loc[idx,'UniProt']=extract_uniprot(df.loc[idx,'OtherIDs'])
-        if j%1000==0: 
-            print ('\n\n\n\n %d completed \n\n\n\n'%j)
-            # pbar.update(100)
-    #TODO   df.to_csv('2022-2-remain_proc_%s_%s_finished.csv'%(mark,j))
-    return df
-pool = multiprocessing.Pool(processes=num_processes)
-result =pool.map(func, chunks)
-f_final=pd.concat(result,sort=False)
-f_final.to_csv('/home/grads/z/zshuying/Documents/shuying/ppi_mutation/scripts/2022-02-variant-sum_remain_deal.csv')
-# print(f_final)
+    num_processes = multiprocessing.cpu_count()
+    print('%s cpus'%num_processes)
+    chunk_size = int(f_simple.shape[0]/num_processes)
+    # global pbar
+    # pbar = tqdm(total=len(f_simple))
+    f_simple['UniProt']=[0]*len(f_simple)
+    chunks = [f_simple.loc[f_simple.index[i:i + chunk_size]] for i in range(0, f_simple.shape[0], chunk_size)]
+    def func(df):
+        mark=df.index[0]
+        print('process %s starts....'%mark, flush=True)
+        for j,idx in enumerate(df.index):
+            if 'UniProt' not in df.loc[idx,'OtherIDs']:
+                    df.loc[idx,'UniProt']=get_uniprot_from_name(f_simple.loc[idx,'Name'])
+            else:df.loc[idx,'UniProt']=extract_uniprot(df.loc[idx,'OtherIDs'])
+            if j%1000==0: 
+                print ('\n\n\n\n %d completed \n\n\n\n'%j, flush=True)
+                # pbar.update(100)
+                df.to_csv('2023-2-round1_proc_%s_%s_finished.csv'%(mark,j))
+        return df
+    pool = multiprocessing.Pool(processes=num_processes)
+    result =pool.map(func, chunks)
+    f_final=pd.concat(result,sort=False)
+    f_final.to_csv('/home/grads/z/zshuying/Documents/shuying/ppi_mutation/scripts/2022-02-variant-sum_remain_deal.csv')
+    # print(f_final)
